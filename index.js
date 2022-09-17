26bb07061172cae7b042b4a5dbd8807c5682504f
Rakib-cloud <ri1641715@gmail.com>
2022-09-18 02:37:21 +0600

    server done

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("<div style='display:flex;height:96vh;justify-content: center;align-items: center;'><h1>Autism Care Server Running!</h1></div>");
+});
+
+const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+
+    try {
+
+        await client.connect();
+        const database = client.db("Autism");
+        const courseCollection = database.collection("courses");
+
+        app.post('/course', async (req, res) => {
+
+            const data = req.body;
+            const result = await courseCollection.insertOne(data);
+            res.json(result);
+
+        });
+
+
+        app.get('/course',async (req,res) => {
+
+            const result = await courseCollection.find({}).toArray()
+            res.send(result);
+
+        })
+
+
+    } finally {
+        // await client.close();
+    }
+
+} run().catch(console.dir);
+
+app.listen(port, () => console.log("response from port", port));
